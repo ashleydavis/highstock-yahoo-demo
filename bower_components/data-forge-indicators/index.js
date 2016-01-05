b56c1a19fa59b3c6d3fbb78df5e647c5b3abe164
Ashley Davis <ashley@codecapers.com.au>
2016-01-05 18:48:43 +1000

    Updated to new version of indicators plugin.

--- bower_components/data-forge-indicators/index.js
+++ bower_components/data-forge-indicators/index.js
@@ -15,30 +15,8 @@ module.exports = function (dataForge) {
     	assert.isNumber(period, "Expected 'period' parameter to 'sma' to be a number that specifies the time period of the moving average.");
 
         var self = this;
-
-        var indices = self.getIndex().toValues();
-        var values = self.toValues();
-        var outputIndices = [];
-        var outputValues = [];
-        for (var i = values.length-1; i > period; --i) {
-            
-            var sample = values[i];
-            var index = indices[i];
-
-            for (var j = 1; j < period; ++j) {
-                sample += values[i-j];
-            }
-
-            sample /= period;
-
-            outputIndices.push(index);
-            outputValues.push(sample);            
-        }
-
-        return new dataForge.Column(
-        	"SMA" + period,
-        	outputValues.reverse(),
-            new dataForge.Index(self.getIndex().getName(), outputIndices.reverse())
-        );
+        return self.rollingWindow(period, function (indices, values) {
+                return [indices[indices.length-1], Enumerable.from(values).sum() / period];
+            });
     };
 };

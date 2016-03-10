d58521219362f0316f2e61b48b07a9ad164a8cde
Ashley Davis <ashley.davis@realseriousgames.com>
2016-03-10 16:15:00 +1000

    Fixed port for Azure deployment.

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ var startServer = function (done) {
 
 	});
 
-	var server = app.listen(3000, function () {
+	var server = app.listen(process.env.PORT || 3000, function () {
 
 		var host = server.address().address
 		var port = server.address().port
